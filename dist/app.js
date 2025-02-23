41cde00b3e1251b90053b162339f868ec405e2c5
Zayed Iqbal <zzayedghost@gmail.com>
2025-02-23 21:30:31 +0600

    adding build v2

--- dist/app.js
+++ dist/app.js
@@ -3,20 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const products_routes_1 = __importDefault(require("./App/features/products/products.routes"));
+const express_1 = __importDefault(require("express"));
 const GlobalErrorHandlers_1 = __importDefault(require("./App/Error/GlobalErrorHandlers"));
-const orders_routes_1 = __importDefault(require("./App/features/orders/orders.routes"));
 const NotFound_1 = __importDefault(require("./App/Error/NotFound"));
 const auth_routes_1 = __importDefault(require("./App/features/Auth/auth.routes"));
+const orders_routes_1 = __importDefault(require("./App/features/orders/orders.routes"));
+const products_routes_1 = __importDefault(require("./App/features/products/products.routes"));
 const admin_routes_1 = __importDefault(require("./App/features/admin/admin.routes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://the-pencil-palace.netlify.app'],
     credentials: true
 }));
 app.use(express_1.default.json());
